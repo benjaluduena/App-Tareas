5392972d362849a3cc7196491390fb0c53a62a4d
benjaluduena <benjaminludluq@gmail.com>
2025-05-26 21:05:54 -0300

    Creacion pantalla Inicio

--- app/src/main/java/com/example/app_tareas/MainActivity.kt
+++ app/src/main/java/com/example/app_tareas/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.example.app_tareas
 
+
+
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.app_tareas.R.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(R.layout.activity_inicio)
+
+        findViewById<BottomNavigationView>(id.bottomNav)
+            .setOnItemSelectedListener { menuItem ->
+                when (menuItem.itemId) {
+                    id.nav_tasks      -> { /* mostrar pantalla de tareas */ true }
+                    id.nav_calendar   -> { /* mostrar calendario */ true }
+                    id.nav_settings   -> { /* mostrar ajustes */ true }
+                    else                -> false
+                }
+            }
     }
 }
---
+++ app/src/main/java/com/example/app_tareas/inicio.kt
@@ -0,0 +1,6 @@
+package com.example.app_tareas
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
